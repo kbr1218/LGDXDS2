18da55fe834606ae495de67371f757dc4c6aabd8
JiwooPark <dxjjjj4@gmail.com>
2025-01-15 17:52:36 +0900

    Update cmmend.jsx

--- cmmend.jsx
+++ cmmend.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './cmm_end.css';
+import './mo_list.css';
 
 const moviecmd = () => {
   return (
@@ -9,18 +9,32 @@ const moviecmd = () => {
         <button className="arow_btnl">&lt;</button>
 
         <div className="box">
-          <img src="/images/peaky.png" alt="example" className="image" />
+          <span class="num">1</span>
+          <img src="/images/peaky.jpg" alt="영화이미지" className="image" />
  
         </div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
+        <div className="box">
+          <span class="num">2</span>
+          <img src="" alt="영화이미지" className="image" />
+        </div>
+          <div className="box">
+          <span class="num">3</span>
+        <img src="" alt="영화이미지" className="image" />
+        </div>
+
+        <div className="box">
+          <span class="num">4</span>
+          <img src="" alt="영화이미지" className="image" />
+        </div>
+          <div className="box">
+          <span class="num">5</span>
+        <img src="" alt="영화이미지" className="image" />
+        </div>
 
         <button className="arow_btnr">&gt;</button>
       </div>
 
-      <button className="restart_btn">다시 추천하기</button>
+      <button className="restart_btn">다시 추천하기</button> 
     </>
   );
 };
