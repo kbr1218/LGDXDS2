8ee076d4602047f9c9bd6bd22f8c97f99bf07ed4
JiwooPark <dxjjjj4@gmail.com>
2025-01-15 17:53:02 +0900

    Create detail.css

---
+++ detail.css
@@ -0,0 +1,27 @@
+import React from 'react';
+import './dtail.css';
+
+const mdetail = () => {
+  return (
+    <div className="m_container">
+      <h1 className="title">영화 제목이 들어가는 부분 </h1>
+      <p className="smry">영화 줄거리 설명이 여기에 들어갑니다. 영화의 간단한 줄거리를 서술해주세요.줄거리줄거리 이 영화의 줄거리는 어떨까 저쩔까 에잇 참나~ </p>
+      
+    <div className="infos">
+    <img src="images/mur.png" alt="영화 이미지" className="eximg" />
+
+      <div className="year"><strong>날짜:</strong> 2025-01-15</div>
+      <div className="gamdok"><strong>감독:</strong> 감독 이름</div>
+      <div className="actors"><strong>출연 배우:</strong> 배우1, 배우2</div>
+      <div className="uage"><strong>연령 제한:</strong> 12세 이상</div>
+      <div className="score"><strong>평점:</strong> 8.5/10</div>
+      <div className="genre"><strong>장르:</strong> 액션, 드라마</div>
+      </div>
+    <div className= "watch_btn">
+      Watch now
+    </div>
+    </div>
+  );
+}
+
+export default mdetail;
