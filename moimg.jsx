db6b648821a577d9645e6dd920de54a03bfa288d
JiwooPark <dxjjjj4@gmail.com>
2025-01-18 17:39:24 +0900

    Create moimg.jsx

---
+++ moimg.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './mo_img.css'; 
+
+const Moimg = () => {
+  return (
+    <>
+      <div className="backdrop_path">
+      </div>
+
+      <div className="poster_path">
+        <img src="/images/mur.png" alt="movie poster" />
+      </div>
+    </>
+  );
+};
+
+export default Moimg;
