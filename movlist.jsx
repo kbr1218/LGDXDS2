002467b7b5ec8043dc9c158bc9f2dc77d4da9306
JiwooPark <dxjjjj4@gmail.com>
2025-01-22 11:01:54 +0900

    Update movlist.jsx

--- movlist.jsx
+++ movlist.jsx
@@ -6,8 +6,6 @@ const moviecmd = () => {
     <>
       <div className="box_container">
 
-        <button className="arow_btnl">&lt;</button>
-
         <div className="box">
           <span class="num">1</span>
           <img src="/images/peaky.jpg" alt="영화이미지" className="image" />
@@ -30,10 +28,10 @@ const moviecmd = () => {
           <span class="num">5</span>
         <img src="" alt="영화이미지" className="image" />
         </div>
-
-        <button className="arow_btnr">&gt;</button>
       </div>
 
+      <button className="arow_btnl">&lt;</button>
+      <button className="arow_btnr">&gt;</button>
       <button className="restart_btn">다시 추천하기</button> 
     </>
   );
